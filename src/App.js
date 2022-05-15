710c15a3d2f6143739934893ca197f45230e2d8d
DaehyeonKim <Daehyeonoff@gmail.com>
2022-05-15 22:56:16 +0900

    20220515

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Back from './components/rspBack';
 
 function App() {
   return (
-    
+    <>
+    <Back></Back>
+    </>
   );
 }
 
--- src/components/rspBack.js
+++ src/components/rspBack.js
@@ -4,7 +4,10 @@ import './rspBack.css';
 const Back = () => {
     return (
         <div className="background">
-
+        <h1 className="title">가위 바위 보!</h1>
         </div>
     )
-}
+};
+
+
+export default Back;
