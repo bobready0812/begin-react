cc5131c48ab23882eb78bf477286994e6e2b743d
DaehyeonKim <Daehyeonoff@gmail.com>
2022-05-16 23:38:40 +0900

    20220516

--- src/components/rspBack.js
+++ src/components/rspBack.js
@@ -6,6 +6,9 @@ const Back = ({children}) => {
         <div className="background">
         <h1 className="title">가위 바위 보!</h1>
         <div>{children}</div>
+        <div className="btn">
+           <h1 className="btnh1">시작</h1>
+        </div>
         </div>
     )
 };
